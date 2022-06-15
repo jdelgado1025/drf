9263454493a1188e11b56be6f3ca1b4a938e5d9b
Jose Delgado <jdelgado1025@gmail.com>
2022-06-14 20:23:34 -0700

    Fixed type in client comment

--- py_client/client.py
+++ py_client/client.py
@@ -5,6 +5,6 @@ endpoint = "http://localhost:8000/api/"
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})
 # print(get_response.text)
 
-#HTTP Reponse
+#HTTP Response
 print(get_response.json())
 print(get_response.status_code)
