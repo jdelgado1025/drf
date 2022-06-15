9404167ed71709c5a3d5c3802e9a6a3be0b689a9
Jose Delgado <jdelgado1025@gmail.com>
2022-06-14 22:03:46 -0700

    usage of drf model serializers to change fields returned

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,19 +3,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
     """
-    Django Rest Framework View
+    Django Rest Framework API View
     """
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        # #Build the dictionary manually from the model
-        # data['id'] = model_data.id
-        # data['title'] = model_data.title
-        # data['content'] = model_data.content
-        # data['price'] = model_data.price
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title', 'price'])
+        data = ProductSerializer(instance).data
     return Response(data)
