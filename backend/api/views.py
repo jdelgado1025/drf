e3f40328c5b9708879fa1b26c2ccc8cf9631479e
Jose Delgado <jdelgado1025@gmail.com>
2022-06-14 21:12:08 -0700

    added comments - manually building dict from model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,12 @@
 import json
-import re
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
 from products.models import Product
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
+    #Build the dictionary manually from the model
     data = {}
     if model_data:
         data['id'] = model_data.id
--- py_client/client.py
+++ py_client/client.py
@@ -2,9 +2,9 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})
+get_response = requests.get(endpoint, json={"product_id": 123})
 # print(get_response.text)
 
 #HTTP Response
 print(get_response.json())
-print(get_response.status_code)
+# print(get_response.status_code)
