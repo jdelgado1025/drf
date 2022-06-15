bd6ed95ccd104dc371c8b037e4bc1a6bb621a43f
Jose Delgado <jdelgado1025@gmail.com>
2022-06-14 21:38:14 -0700

    converted from django view to django rest view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,21 @@
-import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    Django Rest Framework View
+    """
     model_data = Product.objects.all().order_by("?").first()
-    #Build the dictionary manually from the model
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+        # #Build the dictionary manually from the model
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return Response(data)
