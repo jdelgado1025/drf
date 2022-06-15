9b39b277fd02bff60e0e00755f7d0fdf06184f65
Jose Delgado <jdelgado1025@gmail.com>
2022-06-14 20:13:12 -0700

    updated url in client to hit python api and send a generic request

--- py_client/client.py
+++ py_client/client.py
@@ -1,3 +1,10 @@
 import requests
 
-endpoint = ""
+endpoint = "http://localhost:8000/api/"
+
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})
+# print(get_response.text)
+
+#HTTP Reponse
+print(get_response.json())
+print(get_response.status_code)
