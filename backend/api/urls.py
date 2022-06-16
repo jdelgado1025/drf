cb637fa2e0ee988295d8d2bd26f885229ab6a7cb
Jose Delgado <jdelgado1025@gmail.com>
2022-06-15 21:28:42 -0700

    made a note of where all api urls could reside

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from . import views #, include
 
 urlpatterns = [
     path('', views.api_home) #localhost:8000/api
+    #could add api urls here
+    #path('products/', include('products.urls))
 ]
