df0c78c6f19e887ff6403d4d51369bf05709b82e
Jose Delgado <jdelgado1025@gmail.com>
2022-06-15 21:30:24 -0700

    removed yaml import and changed request to POST

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,14 +5,20 @@ from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializer
 
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
     """
     Django Rest Framework API View
     """
     instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
+    """
+    In a GET request, data needs to be a DICT as it is similar to JSON
+    In a POST request, we want the data sent to us
+    """
+    # data = {}
+    serializer = ProductSerializer(data=request.data) #Gets instance of ProductSerializer class and returns data
+    if serializer.is_valid(raise_exception=True):
+        print(serializer.data)
         # data = model_to_dict(instance, fields=['id', 'title', 'price'])
-        data = ProductSerializer(instance).data #Gets instance of ProductSerializer class and returns data
-    return Response(data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
